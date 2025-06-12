e624d7a8fae85bc8bfe1dedf275d9d3e95d04793
ahann01 <ahannn.0127@gmail.com>
2025-06-12 12:28:46 +0800

    membuat class main

---
+++ RumusBeraksi.java
@@ -0,0 +1,14 @@
+package latihan3;
+
+// Nama : Muhammad Farhan, NPM : 2310010700, Kelas : 4C
+
+public class RumusBeraksi {
+    public static void main(String[] args) {
+        // Membuat objek dari class Rumus
+        Rumus farhan = new Rumus(5, 8, 4, 0); // sisi = 5, panjang = 8, lebar = 4
+
+        // Menjalankan method untuk menghitung luas
+        farhan.hitungLuasPersegi();
+        farhan.hitungLuasPersegiPanjang();
+    }
+}
