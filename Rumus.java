4e79f037138bd0eb8fe5517ff75a6f55d5153888
ahann01 <ahannn.0127@gmail.com>
2025-06-12 12:15:44 +0800

    membuat mutator

--- Rumus.java
+++ Rumus.java
@@ -1,5 +1,19 @@
 package latihan3;
   //Nama : Muhammad Farhan, NPM : 2310010700, Kelas : 4c
 public class Rumus {
-    
+    int sisi;
+    int panjang;
+    int lebar;
+    int hasil;
+
+    public void hitungLuasPersegi() {
+        hasil = sisi * sisi;
+        
+        System.out.println("Luas Persegi =" +hasil);
+    }
+     public void hitungLuasPersegiPanjang() {
+        hasil = panjang * lebar;
+        
+        System.out.println("Luas Persegi Panjang =" +hasil);
+     }
 }
